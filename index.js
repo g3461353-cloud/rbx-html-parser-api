3f97760cf5df3e6acd79fc67956b7ecfd0b7b121
g3461353-cloud <g3461353@gmail.com>
2026-03-30 19:19:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,72 +6,54 @@ const app = express();
 const WIDTH = 32; 
 const HEIGHT = 32;
 
-// Loader para permitir que o JSDOM baixe os scripts da Amazon S3
-const resourceLoader = new ResourceLoader({
-    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-});
-
-const playcanvasHTML = `
-    <html>
-    <head>
-        <script src="https://s3-eu-west-1.amazonaws.com/apps.playcanvas.com/DLgXf1zr/playcanvas-stable.min.js"></script>
-        <script>
-            ASSET_PREFIX = "https://s3-eu-west-1.amazonaws.com/apps.playcanvas.com/DLgXf1zr/";
-            SCRIPT_PREFIX = "https://s3-eu-west-1.amazonaws.com/apps.playcanvas.com/DLgXf1zr/";
-            SCENE_PATH = "404993.json";
-            CONTEXT_OPTIONS = { 'antialias': true, 'alpha': false, 'preserveDrawingBuffer': true };
-            SCRIPTS = [ 37175323, 21399876, 4554207, 4554213, 4554214, 4554217, 4554270, 4554271, 4554273, 4554276, 4554279, 20754603, 20755574, 37175329, 37186400 ];
-            CONFIG_FILENAME = "https://s3-eu-west-1.amazonaws.com/apps.playcanvas.com/DLgXf1zr/config.json";
-            INPUT_SETTINGS = { useKeyboard: true, useMouse: true, useTouch: true };
-            pc.script.legacy = false;
-        </script>
-        <script src="https://s3-eu-west-1.amazonaws.com/apps.playcanvas.com/DLgXf1zr/__game-scripts.js"></script>
-    </head>
-    <body>
-        <canvas id="application-canvas" width="${WIDTH}" height="${HEIGHT}"></canvas>
-        <script src="https://s3-eu-west-1.amazonaws.com/apps.playcanvas.com/DLgXf1zr/__start__.js"></script>
-    </body>
-    </html>
-`;
-
-const dom = new JSDOM(playcanvasHTML, {
-    runScripts: "dangerously",
-    resources: resourceLoader,
-    pretendToBeVisual: true
-});
-
-const { window } = dom;
-
-// Mock do Canvas para o Node-Canvas
-const internalCanvas = createCanvas(WIDTH, HEIGHT);
-const internalCtx = internalCanvas.getContext('2d');
-
-// Redireciona o contexto do motor para o nosso buffer
-window.HTMLCanvasElement.prototype.getContext = function(type) {
-    return internalCtx;
+// Sistema de Logs e Status
+let systemLogs = {
+    status: "Iniciando",
+    downloading: false,
+    lastError: "Nenhum",
+    engineLoaded: false,
+    framesProcessed: 0
 };
 
-app.get('/render', (req, res) => {
-    // Captura os pixels gerados pelo motor
-    const imageData = internalCtx.getImageData(0, 0, WIDTH, HEIGHT).data;
-    const pixels = [];
+const resourceLoader = new ResourceLoader({
+    userAgent: "Mozilla/5.0 (Node.js) WebGL-Shim",
+});
 
+// Intercepta o início do download
+resourceLoader.fetch = function(url, options) {
+    systemLogs.downloading = true;
+    systemLogs.status = `Baixando: ${url.split('/').pop()}`;
+    return ResourceLoader.prototype.fetch.call(this, url, options);
+};
+
+const serverCanvas = createCanvas(WIDTH, HEIGHT);
+const serverCtx = serverCanvas.getContext('2d');
+
+app.get('/render', (req, res) => {
+    systemLogs.framesProcessed++;
+    
+    // Captura pixels
+    const imageData = serverCtx.getImageData(0, 0, WIDTH, HEIGHT).data;
+    const pixels = [];
     for (let i = 0; i < imageData.length; i += 4) {
         pixels.push(imageData[i], imageData[i+1], imageData[i+2]);
     }
 
+    // Envia os pixels JUNTO com os logs de sistema
     res.json({
         width: WIDTH,
         height: HEIGHT,
-        data: pixels
+        data: pixels,
+        logs: systemLogs // O Roblox agora recebe isso!
     });
 });
 
-// Endpoint para interagir (pular)
 app.get('/jump', (req, res) => {
-    const event = new window.KeyboardEvent('keydown', { keyCode: 32 }); // Espaço
-    window.dispatchEvent(event);
-    res.send("Jump Sent");
+    systemLogs.status = "Comando de pulo recebido";
+    res.json({ success: true });
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("PlayCanvas WebGL Shim Live"));
+app.listen(process.env.PORT || 3000, () => {
+    systemLogs.status = "Servidor Online";
+    console.log("Sistema de Logs Ativo");
+});
